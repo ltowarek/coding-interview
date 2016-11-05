84b9edabcce085f0718e6fe994878966f6b92f25
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-11-05 17:00:54 +0100

    Solve Palindrome

---
+++ 2_6_palindrome/2_6_palindrome.cpp
@@ -0,0 +1,31 @@
+// Palindrome: Implement a function to check if a linked list is a palindrome.
+
+#include <forward_list>
+#include "gtest/gtest.h"
+
+template <class T>
+bool IsPalindrome(const std::forward_list<T> &list) {
+  auto reversed = list;
+  reversed.reverse();
+  return list == reversed;
+}
+
+TEST(palindrome_test, EmptyList) {
+  const auto list = std::forward_list<int>{};
+  EXPECT_EQ(IsPalindrome(list), true);
+}
+
+TEST(palindrome_test, OneElementList) {
+  const auto list = std::forward_list<int>{1};
+  EXPECT_EQ(IsPalindrome(list), true);
+}
+
+TEST(palindrome_test, NoPalindrome) {
+  const auto list = std::forward_list<int>{1, 2 ,3};
+  EXPECT_EQ(IsPalindrome(list), false);
+}
+
+TEST(palindrome_test, Palindrome) {
+  const auto list = std::forward_list<int>{1, 2 ,1};
+  EXPECT_EQ(IsPalindrome(list), true);
+}
