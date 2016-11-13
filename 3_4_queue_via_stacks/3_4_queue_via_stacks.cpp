1e83c0ecc2607e503c606b9d2fbad6fbd9d05a3f
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-11-13 16:52:27 +0100

    Solve QueueViaSTacks

---
+++ 3_4_queue_via_stacks/3_4_queue_via_stacks.cpp
@@ -0,0 +1,75 @@
+// Queue via Stacks: Implement a MyQueue class which implements a queue using
+// two stacks.
+
+#include <stack>
+#include "gtest/gtest.h"
+
+template <class T>
+class MyQueue {
+public:
+  void Push(const T &data) {
+    s1.push(data);
+  }
+
+  void Pop() {
+    ReverseStack();
+    s2.pop();
+  }
+
+  T Top() {
+    ReverseStack();
+    return s2.top();
+  }
+
+private:
+  void ReverseStack() {
+    if (s2.empty()) {
+      while (!s1.empty()) {
+        s2.push(s1.top());
+        s1.pop();
+      }
+    }
+  }
+
+  std::stack<T> s1;
+  std::stack<T> s2;
+};
+
+TEST(queue_via_stacks_test, PushSingleElement) {
+  auto queue = MyQueue<int>();
+  queue.Push(1);
+  EXPECT_EQ(queue.Top(), 1);
+}
+
+TEST(queue_via_stacks_test, PushMultipleElements) {
+  auto queue = MyQueue<int>();
+  queue.Push(1);
+  queue.Push(2);
+  queue.Push(3);
+  queue.Push(4);
+  EXPECT_EQ(queue.Top(), 1);
+}
+
+TEST(queue_via_stacks_test, PopSingleElement) {
+  auto queue = MyQueue<int>();
+  queue.Push(1);
+  queue.Push(2);
+
+  EXPECT_EQ(queue.Top(), 1);
+  queue.Pop();
+  EXPECT_EQ(queue.Top(), 2);
+}
+
+TEST(queue_via_stacks_test, PopMultipleElements) {
+  auto queue = MyQueue<int>();
+  queue.Push(1);
+  queue.Push(2);
+  queue.Push(3);
+  queue.Push(4);
+
+  EXPECT_EQ(queue.Top(), 1);
+  queue.Pop();
+  EXPECT_EQ(queue.Top(), 2);
+  queue.Pop();
+  EXPECT_EQ(queue.Top(), 3);
+}
